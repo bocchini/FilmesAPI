31102fea713aeef0e1a751de2e2396fe728f3161
Americo Bocchini <americo.bocchini@lambda3.com.br>
2021-07-30 09:27:18 -0300

    Criação do controller filmes e model de filmes

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static readonly List<Filme> filmes = new();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(100, ErrorMessage = "O genero deve ter mais de 100 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 300, ErrorMessage = "A duração deve ter no minimo 1 e no máximo 300 minutos")]
+        public int Duracao { get; set; }
+    }
+}
